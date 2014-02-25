98c31f8265bdcb6eba7c1cd244f75840771b21d6
Neil Clavin <studio@neilclavin.com>
2014-02-25 18:17:47 +0100

    added test class

---
+++ NCTest.h
@@ -0,0 +1,13 @@
+//
+//  NCTest.h
+//  Git Test
+//
+//  Created by Studio on 25/02/14.
+//  Copyright (c) 2014 Neil Clavin Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NCTest : NSObject
+
+@end
---
+++ NCTest.m
@@ -0,0 +1,13 @@
+//
+//  NCTest.m
+//  Git Test
+//
+//  Created by Studio on 25/02/14.
+//  Copyright (c) 2014 Neil Clavin Studio. All rights reserved.
+//
+
+#import "NCTest.h"
+
+@implementation NCTest
+
+@end
